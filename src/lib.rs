c106646852d0b2c3472199b821ca67ba452086bd
Sam Mear <sam.mear2@gmail.com>
2025-07-01 19:58:41 +0100

    bah

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,98 @@ pub trait SpecDbType {
     fn from_hashmap(data: LinkedHashMap<String, Yaml>) -> Self;
 }
 
+#[derive(Clone)]
+struct SpecDbStruct {
+    name: String,
+    part_type: Type,
+    is_part: bool,
+}
+
+#[derive(Clone)]
+pub struct SpecDbFile {
+    file_path: String,
+    contents: String,
+    yaml: Yaml,
+    data: SpecDbStruct
+}
+
+impl SpecDbFile {
+    // fn from_yaml()
+    fn from_file_path(file_path: &String) -> Option<SpecDbFile>
+    {
+        let contents = fs::read_to_string(file_path.clone()).unwrap();
+        println!("{}",file_path.clone().to_string());
+        let parsed_data = YamlLoader::load_from_str(&contents).unwrap()[0].clone();
+        let is_part = match parsed_data["isPart"].as_bool() {
+            Some(is_part) => is_part,
+            None => false,
+        };
+        let part_type = match parsed_data["type"].as_str() {
+            Some(s) => s.to_string(),
+            None => "".to_string(),
+        };
+        let part_name = parsed_data["name"].as_str().expect(format!("Missing required name. Or it is not a string. File: {}", file_path).as_str());
+        let release_date = parsed_data["data"]["Release Date"].as_str();
+        let inherits = match parsed_data["inherits"].as_vec() {
+            Some(_) => true,
+            None => false,
+        };
+        if inherits {
+            println!("Ignoring {} for now, will parse when all hidden types are parsed.", part_name)
+        } else {
+
+            println!("Parsing {}", part_name);
+
+            let struct_object = SpecDbStruct {
+                name: part_name.to_owned(),
+                part_type: Type::from_yaml(part_type, &parsed_data).expect(format!("Invalid part type in file: {}", file_path).as_str()),
+                is_part: is_part
+            };
+
+
+            
+            let bah = SpecDbFile {
+                file_path: file_path.clone(),
+                contents: contents.clone(),
+                yaml: YamlLoader::load_from_str(&contents).unwrap()[0].clone(),
+                data: struct_object
+            };
+            return Some(bah);
+        }
+        None
+    }
+
+    fn from_file_path_and_inherit(file_path: &String, mut data: LinkedHashMap<String, Yaml>) -> Option<SpecDbFile>
+    {
+        let contents = fs::read_to_string(file_path.clone()).unwrap();
+        println!("{}",file_path.clone().to_string());
+        let parsed_data = YamlLoader::load_from_str(&contents).unwrap()[0].clone();
+        let is_part = match parsed_data["isPart"].as_bool() {
+            Some(is_part) => is_part,
+            None => false,
+        };
+        let part_type = match parsed_data["type"].as_str() {
+            Some(s) => s.to_string(),
+            None => "".to_string(),
+        };
+        let part_name = parsed_data["name"].as_str().expect(format!("Missing required name. Or it is not a string. File: {}", file_path).as_str());
+        let main_data = parsed_data["data"].as_hash().unwrap();
+        for data_element in main_data.iter() {
+            data.insert(data_element.0.as_str().expect("expected string").to_string(), data_element.1.clone());
+        }
+
+        let mut struct_object = SpecDbStruct {
+            name: part_name.to_owned(),
+            part_type: Type::from_hashmap(part_type, data).expect(format!("Invalid part type in file: {}", file_path).as_str()),
+            is_part: is_part
+        };
+        let bah = SpecDbFile {
+            file_path: file_path.clone(),
+            contents: contents.clone(),
+            yaml: YamlLoader::load_from_str(&contents).unwrap()[0].clone(),
+            data: struct_object
+        };
+        return Some(bah);
+    }
+}
+
--- src/spectype/mod.rs
+++ src/spectype/mod.rs
@@ -11,3 +11,92 @@ pub use cpu_architecture::CpuArchitecture;
 pub use graphics_architecture::GraphicsArchitecture;
 pub use apu_architecture::ApuArchitecture;
 pub use apu::Apu;
+use hashlink::LinkedHashMap;
+use yaml_rust2::Yaml;
+
+use crate::SpecDbType;
+
+// todo: turn each one of these enum variants into a struct
+// easy way to tell which type requires what data, and what data are optional.
+#[derive(Clone)]
+pub enum Type {
+    Cpu(Cpu),
+    Apu(Apu),
+    GraphicsCard(GraphicsCard),
+    CpuArchitecture(CpuArchitecture),
+    ApuArchitecture(ApuArchitecture),
+    GraphicsArchitecture(GraphicsArchitecture),
+    GenericContainer,
+    Hidden
+}
+
+impl Type {
+    pub fn from_yaml(label: String, parsed_data: &Yaml) -> Option<Self>
+    {
+        if "CPU".to_string() == label {
+            return Some(Self::Cpu(Cpu::from_yaml(&parsed_data["data"])));
+        }
+        else if "APU".to_string() == label {
+            return Some(Self::Apu(Apu::from_yaml(&parsed_data["data"])));
+        }
+        if "Graphics Card".to_string() == label {
+            return Some(Self::GraphicsCard(GraphicsCard::from_yaml(&parsed_data["data"])));
+        }
+        if "CPU Architecture".to_string() == label {
+            return Some(Self::CpuArchitecture(CpuArchitecture::from_yaml(&parsed_data["data"])));
+        }
+        if "APU Architecture".to_string() == label {
+            return Some(Self::ApuArchitecture(ApuArchitecture::from_yaml(&parsed_data["data"])));
+        }
+        if "Graphics Architecture".to_string() == label {
+            return Some(Self::GraphicsArchitecture(GraphicsArchitecture::from_yaml(&parsed_data["data"])));
+        }
+        if "Generic Container".to_string() == label {
+            return Some(Self::GenericContainer);
+        }
+        if "Hidden".to_string() == label {
+            return Some(Self::Hidden);
+        }
+        return None;
+    }
+    pub fn from_hashmap(label: String, data: LinkedHashMap<String, Yaml>) -> Option<Self>
+    {
+        if "CPU".to_string() == label {
+            return Some(Self::Cpu(Cpu::from_hashmap(data)));
+        }
+        else if "APU".to_string() == label {
+            return Some(Self::Apu(Apu::from_hashmap(data)));
+        }
+        if "Graphics Card".to_string() == label {
+            return Some(Self::GraphicsCard(GraphicsCard::from_hashmap(data)));
+        }
+        if "CPU Architecture".to_string() == label {
+            return Some(Self::CpuArchitecture(CpuArchitecture::from_hashmap(data)));
+        }
+        if "APU Architecture".to_string() == label {
+            return Some(Self::ApuArchitecture(ApuArchitecture::from_hashmap(data)));
+        }
+        if "Graphics Architecture".to_string() == label {
+            return Some(Self::GraphicsArchitecture(GraphicsArchitecture::from_hashmap(data)));
+        }
+        if "Generic Container".to_string() == label {
+            return Some(Self::GenericContainer);
+        }
+        if "Hidden".to_string() == label {
+            return Some(Self::Hidden);
+        }
+        return None;
+    }
+    pub fn label(&self) -> String {
+        match self {
+            Type::Cpu(_) => "CPU".to_string(),
+            Type::Apu(_) => "APU".to_string(),
+            Type::GraphicsCard(_) => "Graphics Card".to_string(),
+            Type::CpuArchitecture(_) => "CPU Architecture".to_string(),
+            Type::ApuArchitecture(_) => "APU Architecture".to_string(),
+            Type::GraphicsArchitecture(_) => "Graphics Architecture".to_string(),
+            Type::GenericContainer => "Generic Container".to_string(),
+            Type::Hidden => "Hidden".to_string(),
+        }
+    }
+}
