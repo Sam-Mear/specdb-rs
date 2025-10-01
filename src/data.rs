8a44b5c1cb2f596d4f68b735d204fe02cd68cd3b
Sam Mear <sam.mear@brandaddition.com>
2025-10-01 18:55:04 +0100

    full graphics card specs

--- src/data.rs
+++ src/data.rs
@@ -6,281 +6,326 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize)]
 pub struct Lithography (pub String);
 
-#[Scalar]
-impl ScalarType for Lithography {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(Lithography(s.to_owned()))
-    } else {
-        Err("Invalid value for Lithography".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
-
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct ReleaseDate (pub String);
 
-#[Scalar]
-impl ScalarType for ReleaseDate {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(ReleaseDate(s.to_owned()))
-    } else {
-        Err("Invalid value for ReleaseDate".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
-
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct Sockets (pub Vec<String>);
-
-#[Scalar]
-impl ScalarType for Sockets {
-    fn parse(value: Value) -> InputValueResult<Self> { 
-        if let Value::List(l) = &value {
-            let mut sockets = Vec::new();
-            for item in l {
-                if let Value::String(s) = item {
-                    sockets.push(s.to_owned());
-                } else {
-                    return Err("Invalid value in Sockets array".into());
-                }
-            }
-            Ok(Sockets(sockets))
-        } else {
-            Err("Invalid value for Sockets".into())
-        }
-    }
-    
-    fn to_value(&self) -> Value { 
-        let list: Vec<Value> = self.0.iter().map(|s| Value::String(s.to_owned())).collect();
-        Value::List(list)
-    }
-}
 
 
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct CoreCount (pub u16);
 
-#[Scalar]
-impl ScalarType for CoreCount {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::Number(n) = &value {
-        if let Some(u) = n.as_u64() {
-            if u <= u16::MAX as u64 {
-                return Ok(CoreCount(u as u16));
-            }
-        }
-    }
-    Err("Invalid value for CoreCount".into())
-  }
-
-  fn to_value(&self) -> Value { Value::Number(self.0.into()) }
-}
-
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct ThreadCount (pub u16);
-
-#[Scalar]
-impl ScalarType for ThreadCount {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::Number(n) = &value {
-        if let Some(u) = n.as_u64() {
-            if u <= u16::MAX as u64 {
-                return Ok(ThreadCount(u as u16));
-            }
-        }
-    }
-    Err("Invalid value for ThreadCount".into())
-  }
-
-  fn to_value(&self) -> Value { Value::Number(self.0.into()) }
-}
 
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct BaseFrequency (pub String);
 
-#[Scalar]
-impl ScalarType for BaseFrequency {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(BaseFrequency(s.to_owned()))
-    } else {
-        Err("Invalid value for BaseFrequency".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
-
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct Tdp (pub String);
-
-#[Scalar]
-impl ScalarType for Tdp {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(Tdp(s.to_owned()))
-    } else {
-        Err("Invalid value for Tdp".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
 
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct VramCapacity (pub String);
 
-#[Scalar]
-impl ScalarType for VramCapacity {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(VramCapacity(s.to_owned()))
-    } else {
-        Err("Invalid value for VramCapacity".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
-
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct ShaderProcessorCount (pub u32);
-
-#[Scalar]
-impl ScalarType for ShaderProcessorCount {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::Number(n) = &value {
-        if let Some(u) = n.as_u64() {
-            if u <= u32::MAX as u64 {
-                return Ok(ShaderProcessorCount(u as u32));
-            }
-        }
-    }
-    Err("Invalid value for ShaderProcessorCount".into())
-  }
-
-  fn to_value(&self) -> Value { Value::Number(self.0.into()) }
-}
 
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct GpuBaseFrequency(pub String);
 
-#[Scalar]
-impl ScalarType for GpuBaseFrequency {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(GpuBaseFrequency(s.to_owned()))
-    } else {
-        Err("Invalid value for GpuBaseFrequency".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
-
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct Manufacturer(pub String);
-
-#[Scalar]
-impl ScalarType for Manufacturer {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(Manufacturer(s.to_owned()))
-    } else {
-        Err("Invalid value for Manufacturer".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
 
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct Vendor(pub String);
 
-#[Scalar]
-impl ScalarType for Vendor {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(Vendor(s.to_owned()))
-    } else {
-        Err("Invalid value for Vendor".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
 
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct Architecture(pub String);
-
-#[Scalar]
-impl ScalarType for Architecture {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::String(s) = &value {
-        Ok(Architecture(s.to_owned()))
-    } else {
-        Err("Invalid value for Architecture".into())
-    }
-  }
-
-  fn to_value(&self) -> Value { Value::String(self.0.to_owned()) }
-}
 
 
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct TensorCores (pub u16);
 
-#[Scalar]
-impl ScalarType for TensorCores {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::Number(n) = &value {
-        if let Some(u) = n.as_u64() {
-            if u <= u16::MAX as u64 {
-                return Ok(TensorCores(u as u16));
-            }
-        }
-    }
-    Err("Invalid value for TensorCores".into())
-  }
-
-  fn to_value(&self) -> Value { Value::Number(self.0.into()) }
-}
-
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct RayTracingCores (pub u16);
 
-#[Scalar]
-impl ScalarType for RayTracingCores {
-  fn parse(value: Value) -> InputValueResult<Self> { 
-    if let Value::Number(n) = &value {
-        if let Some(u) = n.as_u64() {
-            if u <= u16::MAX as u64 {
-                return Ok(RayTracingCores(u as u16));
+// These suppots are either int.int or 'N/A'
+// todo: account for this
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct DirectXSupport(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct OpenGLSupport(pub String);
+
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct OpenCLSupport(pub String);
+
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct VulkanSupport(pub String);
+
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct Market(pub Vec<String>);
+
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct HardwareAcceleratedEncoding(pub Vec<String>);
+
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct HardwareAcceleratedDecoding(pub Vec<String>);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct PowerConnectors(pub Vec<String>);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct Outputs(pub Vec<String>);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct VramFrequency(pub String);
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct VramType(pub String);
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct VramBandwidth(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct VramBusWidth(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct RenderOutputUnitCount(pub u16);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct TextureMappingUnitCount(pub u16);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct DieSize(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct Gpu(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct GpuVariant(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct HlslShaderModel(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct GpuBoostFrequency(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct FP32Compute(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct FP64Compute(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct SlotWidth(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct Length(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct Height(pub String);
+
+
+#[derive(Clone)]
+#[derive(Serialize)]
+pub struct Width(pub String);
+
+macro_rules! impl_scalar_string {
+    ($name:ident) => {
+        #[Scalar]
+        impl ScalarType for $name {
+            fn parse(value: Value) -> InputValueResult<Self> {
+                if let Value::String(s) = &value {
+                    Ok($name(s.to_owned()))
+                } else {
+                    Err(format!("Invalid value for {}", stringify!($name)).into())
+                }
+            }
+            fn to_value(&self) -> Value {
+                Value::String(self.0.to_owned())
             }
         }
-    }
-    Err("Invalid value for RayTracingCores".into())
-  }
-
-  fn to_value(&self) -> Value { Value::Number(self.0.into()) }
+    };
 }
+
+macro_rules! impl_scalar_u16 {
+    ($name:ident) => {
+        #[Scalar]
+        impl ScalarType for $name {
+            fn parse(value: Value) -> InputValueResult<Self> {
+                if let Value::Number(n) = &value {
+                    if let Some(u) = n.as_u64() {
+                        if u <= u16::MAX as u64 {
+                            return Ok($name(u as u16));
+                        }
+                    }
+                }
+                Err(format!("Invalid value for {}", stringify!($name)).into())
+            }
+            fn to_value(&self) -> Value {
+                Value::Number(self.0.into())
+            }
+        }
+    };
+}
+
+macro_rules! impl_scalar_u32 {
+    ($name:ident) => {
+        #[Scalar]
+        impl ScalarType for $name {
+            fn parse(value: Value) -> InputValueResult<Self> {
+                if let Value::Number(n) = &value {
+                    if let Some(u) = n.as_u64() {
+                        if u <= u32::MAX as u64 {
+                            return Ok($name(u as u32));
+                        }
+                    }
+                }
+                Err(format!("Invalid value for {}", stringify!($name)).into())
+            }
+            fn to_value(&self) -> Value {
+                Value::Number(self.0.into())
+            }
+        }
+    };
+}
+
+macro_rules! impl_scalar_vec_string {
+    ($name:ident) => {
+        #[Scalar]
+        impl ScalarType for $name {
+            fn parse(value: Value) -> InputValueResult<Self> {
+                if let Value::List(l) = &value {
+                    let mut items = Vec::new();
+                    for item in l {
+                        if let Value::String(s) = item {
+                            items.push(s.to_owned());
+                        } else {
+                            return Err(format!("Invalid value in {} array", stringify!($name)).into());
+                        }
+                    }
+                    Ok($name(items))
+                } else {
+                    Err(format!("Invalid value for {}", stringify!($name)).into())
+                }
+            }
+            fn to_value(&self) -> Value {
+                let list: Vec<Value> = self.0.iter().map(|s| Value::String(s.to_owned())).collect();
+                Value::List(list)
+            }
+        }
+    };
+}
+
+
+impl_scalar_string!(Lithography);
+impl_scalar_string!(ReleaseDate);
+impl_scalar_vec_string!(Sockets);
+impl_scalar_u16!(CoreCount);
+impl_scalar_u16!(ThreadCount);
+impl_scalar_string!(BaseFrequency);
+impl_scalar_string!(Tdp);
+impl_scalar_string!(VramCapacity);
+impl_scalar_u32!(ShaderProcessorCount);
+impl_scalar_string!(GpuBaseFrequency);
+impl_scalar_string!(Manufacturer);
+impl_scalar_string!(Vendor);
+impl_scalar_string!(Architecture);
+impl_scalar_u16!(TensorCores);
+impl_scalar_u16!(RayTracingCores);
+impl_scalar_string!(DirectXSupport);
+impl_scalar_string!(OpenGLSupport);
+impl_scalar_string!(OpenCLSupport);
+impl_scalar_string!(VulkanSupport);
+impl_scalar_vec_string!(Market);
+impl_scalar_vec_string!(HardwareAcceleratedEncoding);
+impl_scalar_vec_string!(HardwareAcceleratedDecoding);
+impl_scalar_vec_string!(PowerConnectors);
+impl_scalar_vec_string!(Outputs);
+impl_scalar_string!(VramFrequency);
+impl_scalar_string!(VramType);
+impl_scalar_string!(VramBandwidth);
+impl_scalar_string!(VramBusWidth);
+impl_scalar_u16!(RenderOutputUnitCount);
+impl_scalar_u16!(TextureMappingUnitCount);
+impl_scalar_string!(DieSize);
+impl_scalar_string!(Gpu);
+impl_scalar_string!(GpuVariant);
+impl_scalar_string!(HlslShaderModel);
+impl_scalar_string!(GpuBoostFrequency);
+impl_scalar_string!(FP32Compute);
+impl_scalar_string!(FP64Compute);
+impl_scalar_string!(SlotWidth);
+impl_scalar_string!(Length);
+impl_scalar_string!(Height);
+impl_scalar_string!(Width);
+
